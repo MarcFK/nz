022527d95aebd30675f0b70353e3d6ffba099b60
MarcFK <marc.kaufer@student.uibk.ac.at>
2024-03-21 17:50:45 +0100

    map

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-<script>
-    var map = L.map('map').setView([-43.5, 170.22], 13);
+var map = L.map('map').setView([-43.463, 170.19], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+}).addTo(map);
 
-    var marker = L.marker([28.27273, -16.64235]).addTo(map);
+var marker = L.marker([-43.463, 170.19]).addTo(map);
 
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-        circle.bindPopup("I am a circle.");
-        polygon.bindPopup("I am a polygon.");
-</script>
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
