c995d7aa8e9b700bd27eba163a0a662f389102ff
MarcFK <marc.kaufer@student.uibk.ac.at>
2024-04-11 15:55:56 +0200

    var mit let ersetzt

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var map = L.map('map').setView([-43.463, 170.19], 13);
+let map = L.map('map').setView([-43.463, 170.19], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-43.463, 170.19]).addTo(map);
+let marker = L.marker([-43.463, 170.19]).addTo(map);
 
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
