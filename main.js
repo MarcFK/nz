51f8db52f499dfff75639d931701f4d2395ddbf3
MarcFK <marc.kaufer@student.uibk.ac.at>
2024-04-11 16:14:56 +0200

    popup

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ let lat = -43.463;
 let lng = 170.19;
 let zoom = 13
 
-let map = L.map('map').setView([-43.463, 170.19], 13);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-43.463, 170.19]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+marker.bindPopup(`
+    <h2>Franz Josef Glacier</h2>
+    <ul>
+        <li>Breite: ${lat.toFixed(5)}</li>
+        <li>Länge: ${lng.toFixed(5)}</li>
+    </ul>
+`).openPopup();
